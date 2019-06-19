23ee2c93f1a3942e1fc924f8cc97fd5ad350369c
huwf <huwf@liandisys.com.cn>
2019-06-19 17:28:29 +0800

    20190619

---
+++ src/main/java/spider/api/SpiderApiApplication.java
@@ -0,0 +1,13 @@
+package spider.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpiderApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpiderApiApplication.class, args);
+	}
+
+}
