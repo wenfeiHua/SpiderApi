98f37293a291427252960e087c2b5fc3df9ed915
huwf <huwf@liandisys.com.cn>
2019-06-20 16:52:35 +0800

    数据库的连接以及代码修改 20190620003

--- src/main/java/spider/api/ErrorMessage.java
+++ src/main/java/spider/api/ErrorMessage.java
@@ -20,12 +20,4 @@ public class ErrorMessage {
 	public static final String ERROR_CODE004 = "E004";
 
 	public static final String ERROE_DETAIL004 = "数据删除失败";
-
-	public static final String ERROR_CODE005 = "E005";
-
-	public static final String ERROE_DETAIL005 = "用户名已存在，重新输入。";
-
-	public static final String ERROR_CODE006 = "E006";
-
-	public static final String ERROE_DETAIL006 = "账号已存在，重新输入。";
 }
